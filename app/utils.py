34ebc2c7ab8b2de548bdb43ca1fcdf579412fcb6
Kizito Okorie <kyzeokorie22@gmail.com>
2022-07-28 11:37:19 +0200

    Updating changes to the caption

--- app/utils.py
+++ app/utils.py
@@ -35,7 +35,7 @@ def head():
 
     st.caption("""
          <p style='text-align: center'>
-         by <a href='https://medium.com/geoclid'>Geoclid</a>
+         by <a href='https://medium.com/geoclid'>Kizito'.'Okorie</a>
          </p>
     """, unsafe_allow_html=True)
 
